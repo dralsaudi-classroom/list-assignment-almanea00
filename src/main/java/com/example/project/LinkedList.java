e871aa40907c4e62de90819226daea7c66fcfd0a
abdulelah almanea <a.almanea922@gmail.com>
2024-09-21 14:09:14 +0300

    Add files via upload

--- src/main/java/com/example/project/LinkedList.java
+++ src/main/java/com/example/project/LinkedList.java
@@ -1,66 +1,110 @@
 package com.example.project;
 
-public class LinkedList<T> implements List<T>{
+import java.util.HashMap;
+
+public class LinkedList<T> implements List<T> {
 	private Node<T> head;
 	private Node<T> current;
-	
-    public LinkedList() {
+
+	public LinkedList() {
 		head = current = null;
 	}
+
 	public boolean empty() {
 		return head == null;
 	}
+
 	public boolean last() {
 		return current.next == null;
 	}
-    public boolean full() {
-        return false;
-    }
-    public void findFirst() {
-        current = head;
-    }
-    public void findNext() {
-        current = current.next;
-    }
-    public T retrieve() {
-        return current.data;
-    }
-    public void update(T e) {
-        current.data = e;
-    }
-    public void insert(T e) {
-        if (empty()) {
-            current = head = new Node<T>(e);
-        } else {
-            Node<T> tmp = current.next;
-            current.next = new Node<T>(e);
-            current = current.next;
-            current.next = tmp;
-        }
-    }
-    public void remove() {
-        if (current == head) {
-            head = head.next;
-        } else {
-            Node<T> tmp = head;
-            while (tmp.next != current) {
-                tmp = tmp.next;
-            }
-            tmp.next = current.next;
-        }
-        if (current.next == null) {
-            current = head;
-        } else {
-            current = current.next;
-        }
-    }
-    public T mostFrequentElement() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //         Write the method mostFrequentElement, member of the class LinkedList, that returns
-        // the most frequent element in the list. The most frequent element is the element
-        // appearing the highest number of times. If one or more element appear the same
-        // number of times, the one encountered earlier is returned.
-        // Example 1.1. Given the list l : A, B, C, B, C, D, E, mostFrequentElement() returns
-        // B.
-    }
+
+	public boolean full() {
+		return false;
+	}
+
+	public void findFirst() {
+		current = head;
+	}
+
+	public void findNext() {
+		current = current.next;
+	}
+
+	public T retrieve() {
+		return current.data;
+	}
+
+	public void update(T e) {
+		current.data = e;
+	}
+
+	public void insert(T e) {
+		if (empty()) {
+			current = head = new Node<T>(e);
+		} else {
+			Node<T> tmp = current.next;
+			current.next = new Node<T>(e);
+			current = current.next;
+			current.next = tmp;
+		}
+	}
+
+	public void remove() {
+		if (current == head) {
+			head = head.next;
+		} else {
+			Node<T> tmp = head;
+			while (tmp.next != current) {
+				tmp = tmp.next;
+			}
+			tmp.next = current.next;
+		}
+		if (current.next == null) {
+			current = head;
+		} else {
+			current = current.next;
+		}
+	}
+
+	public T mostFrequentElement() {
+		HashMap<T, Integer> freq = new HashMap<>();
+
+		if (head == null)
+			return null;
+
+		current = head;
+		T i;
+		
+		while (current != null) {
+			i = current.data;
+			if (freq.containsKey(i)) 
+				freq.put(i, freq.get(i)+1);
+			
+			else {
+				freq.put(i, 1);
+			}
+
+			current = current.next;
+		}
+
+		T key = head.data;
+		int freqMax = freq.get(head.data);
+
+		current = head.next;
+		while (current != null) {
+			i = current.data;
+			
+			if (freq.get(i) > freqMax) {
+				freqMax = freq.get(i);
+				key = i;
+			}
+				
+			
+			current = current.next;
+		}
+
+		return key;
+
+	}
+
 }
--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -3,20 +3,54 @@ package com.example.project;
 public class ListTester {
     public static <T> void circularLeftShift(List <T> list, int n)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the method circularLeftShift, user of List ADT, that takes as input a non-empty
-        // List list and an integer n > 0 and performs n circular left shift of the list.
-        // Example 2.1. Given the list l : A, B, C, D, E, circularShiftLeft(l, 1) results in
-        // B, C, D, E, A, circularShiftLeft(l, 2) results in C, D, E, A, B.
+    	if(list.empty())
+    		return;
+    	
+    	int size = 1;
+    	list.findFirst();
+    	
+        while(!list.last()) {
+        	list.findNext();
+        	size++;
+        }
+        
+        T[] temp = (T[]) new Object[size];
+        
+        int counter = 0;
+        list.findFirst();
+        
+        while(!list.last()) {
+        	temp[(counter+n*(size-1)) % size] = list.retrieve();
+        	list.findNext();
+        	counter++;
+        }
+        
+        temp[(counter+n*(size-1)) % size] = list.retrieve();
+        
+        counter = 0;
+        list.findFirst();
+        
+        while(!list.last()) {
+        	list.update(temp[counter]);
+        	list.findNext();
+        	counter++;
+        }
+        list.update(temp[counter]);
     }
     public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the method reverseCopy, user of DoubleLinkedList, which copies the elements
-        // of l1 to l2 in reverse order. The list l1 must not change. Assume that l2 is empty.
-        // The method signature is public static <T> void reverseCopy(DoubleLinkedList<T> l1,
-        // DoubleLinkedList<T> l2).
-        // Example 4.1. If l1 : A ↔ B ↔ C ↔ D, then calling reverseCopy(l1, l2) results in
-        // l2 : D ↔ C ↔ B ↔ A.
+    	if (l1.empty())
+			return;
+
+		while (!l1.last()) {
+			l1.findNext();
+		}
+
+		l2.findFirst();
+		while (!l1.first()) {
+			l2.insert(l1.retrieve());
+			l1.findPrevious();
+		}
+		l2.insert(l1.retrieve());
     }
 }
